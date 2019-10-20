1b28777d973fa45e682757daa0341b5c97a5e0f4
ardiwsaputra <ardiwidyanto15@gmail.com>
2019-10-20 17:32:12 +0700

    Update User Seeder

--- seeders/20191014054310-demo-user.js
+++ seeders/20191014054310-demo-user.js
@@ -3,7 +3,7 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert(
-      'Users',
+      'users',
       [
         {
           email: 'johni@gmail.com',
