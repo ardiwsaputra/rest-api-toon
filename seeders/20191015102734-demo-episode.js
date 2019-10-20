23ac50ad9e98a6356deac1b5e532b9a40e356b53
ardiwsaputra <ardiwidyanto15@gmail.com>
2019-10-20 17:35:00 +0700

    Update All Seeder

--- seeders/20191015102734-demo-episode.js
+++ seeders/20191015102734-demo-episode.js
@@ -3,7 +3,7 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert(
-      'Episodes',
+      'episodes',
       [
         {
           title: 'Ep.1 Naruto Kembali',
