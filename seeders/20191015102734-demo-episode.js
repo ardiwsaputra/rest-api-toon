ff92e3c2e6449e222ef0db7eab964f1d96774d6c
ardiwsaputra <ardiwidyanto15@gmail.com>
2019-10-24 09:10:44 +0700

    Update Seeder Episode

--- seeders/20191015102734-demo-episode.js
+++ seeders/20191015102734-demo-episode.js
@@ -6,21 +6,39 @@ module.exports = {
       'episodes',
       [
         {
-          title: 'Ep.1 Naruto Kembali',
+          title: 'Ep.1 Perjuangan tanpa Batas',
           image:
-            'https://www.pixelstalk.net/wp-content/uploads/2016/05/Chidori-Sasuke-Wallpaper.jpg',
+            'https://i0.wp.com/lh3.googleusercontent.com/-uhAGBYL8Lgc/XICkzNTy6KI/AAAAAAAADEE/2VA_t1jcw-s7WGAXa2EHwWMCAcF3qTj0QCLcBGAs/s1600/40.9y9VoHk2.jpg?resize=170,130&quality=80',
           createdAt: new Date(),
           updatedAt: new Date(),
-          webtoonId: 1,
+          webtoonId: 4,
+          createdBy: 2,
+        },
+        {
+          title: 'Ep.1 Karier dimulai',
+          image:
+            'https://i0.wp.com/komiku.co/wp-content/uploads/Forever-Mine-1.jpg?resize=95,150&quality=80',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          webtoonId: 5,
+          createdBy: 2,
+        },
+        {
+          title: 'Ep.1 Badai Menerjang',
+          image:
+            'https://i0.wp.com/komiku.co/wp-content/uploads/Forever-Mine-1.jpg?resize=95,150&quality=80',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          webtoonId: 2,
           createdBy: 1,
         },
         {
-          title: 'Ep.2 Sasuke Kembali',
+          title: 'Ep.2 Tanpa Batas',
           image:
-            'https://www.pixelstalk.net/wp-content/uploads/2016/05/Chidori-Sasuke-Wallpaper.jpg',
+            'https://i0.wp.com/lh3.googleusercontent.com/-uhAGBYL8Lgc/XICkzNTy6KI/AAAAAAAADEE/2VA_t1jcw-s7WGAXa2EHwWMCAcF3qTj0QCLcBGAs/s1600/40.9y9VoHk2.jpg?resize=170,130&quality=80',
           createdAt: new Date(),
           updatedAt: new Date(),
-          webtoonId: 1,
+          webtoonId: 2,
           createdBy: 1,
         },
       ],
